f04df4c582c03823046052d75d9e5562f5e4c2d3
solitarywalk <bilcuee@gmail.com>
2017-04-24 15:36:39 +0900

    sticky header 기능 추가

--- asset/js/solitarywalk.js
+++ asset/js/solitarywalk.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
+
+  var stickyOffset = $('.wrapper-masthead').offset().top + $('.wrapper-masthead').height() - 50 ;
   
   //Check to see if the window is top if not then display button
   $(window).scroll(function(){
+
     if ($(this).scrollTop() > 100) {
       $('.scrollToTop').fadeIn();
     } else {
       $('.scrollToTop').fadeOut();
     }
+
+    var sticky = $('.wrapper-masthead'),
+        scroll = $(window).scrollTop();
+
+    if (scroll >= stickyOffset) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+
   });
+
   
   //Click event to scroll to top
   $('.scrollToTop').click(function(){
